8d751e670e2ad1aa569ccb1a83230ea28a92275b
Jackie Tan <free-soup@hotmail.com>
2019-02-12 14:39:55 +0800

    Add Utility class to help parse expressions for string matching

---
+++ Code/source/ExpressionUtil.cpp
@@ -0,0 +1,10 @@
+#include <string>
+
+using namespace std;
+
+#include "ExpressionUtil.h"
+
+//Convert Infix Expressions into Postfix expression for easier string matching
+bool convertInfixToPostfix(std::string expression) {
+	return true;
+}
