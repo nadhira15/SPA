59407ca99f09354220d5ffdef7017816794def48
randypx <sinrandy@hotmail.com>
2019-02-24 15:16:58 +0800

    PKB now stores lists of statement types (read, print, assign, if, while), variables and constants as well as the procedure name

--- Code/source/PKB.cpp
+++ Code/source/PKB.cpp
@@ -1,19 +1,93 @@
 #pragma once
 
-#include<stdio.h>
-#include <iostream>
-#include <string>
-#include <vector>
-
-using namespace std;
-
 #include "PKB.h"
-#include "TNode.h"
 
-int PKB::setProcToAST(PROC p, TNode* r) {
-	return NULL;
+PKB::PKB()
+{
 }
 
-TNode* PKB::getRootAST (PROC p){
-	return NULL;
+void PKB::addProc(string name)
+{
+	procName = name;
+}
+
+void PKB::addStatement(int stmNo, stmType type)
+{
+	stmTypeList[stmNo] = type;
+
+	switch (type)
+	{
+		case read:
+			readStmList.emplace(stmNo);
+			break;
+		case print:
+			printStmList.emplace(stmNo);
+			break;
+		case assign:
+			assignStmList.emplace(stmNo);
+			break;
+		case whileStm:
+			whileStmList.emplace(stmNo);
+			break;
+		case ifStm:
+			ifStmList.emplace(stmNo);
+			break;
+		default:
+			break;
+	}
+}
+
+void PKB::addVariable(string name)
+{
+	varList.push_back(name);
+}
+
+void PKB::addConstant(int value)
+{
+	constList.push_back(value);
+}
+
+string PKB::getProcName()
+{
+	return procName;
+}
+
+int PKB::getTotalStmNo()
+{
+	return stmTypeList.size();
+}
+
+unordered_set<int> PKB::getReadStms()
+{
+	return readStmList;
+}
+
+unordered_set<int> PKB::getPrintStms()
+{
+	return printStmList;
+}
+
+unordered_set<int> PKB::getAssignStms()
+{
+	return assignStmList;
+}
+
+unordered_set<int> PKB::getIfStms()
+{
+	return ifStmList;
+}
+
+unordered_set<int> PKB::getWhileStms()
+{
+	return whileStmList;
+}
+
+vector<string> PKB::getVariables()
+{
+	return varList;
+}
+
+vector<int> PKB::getConstants()
+{
+	return constList;
 }
--- Code/source/PKB.h
+++ Code/source/PKB.h
@@ -1,21 +1,40 @@
 #pragma once
 
-#include<stdio.h>
-#include <iostream>
 #include <string>
+#include <unordered_set>
 #include <vector>
 
 using namespace std;
-typedef short PROC;
 
-class TNode;
-
-class VarTable;  // no need to #include "VarTable.h" as all I need is pointer
+enum stmType { read, print, assign, whileStm, ifStm };
 
 class PKB {
 public:
-	static VarTable* varTable; 
-	static int setProcToAST(PROC p, TNode* r);
-	static TNode* getRootAST (PROC p);
+	PKB();
 
+	void addProc(string procName);
+	void addStatement(int stmNo, stmType type);
+	void addVariable(string name);
+	void addConstant(int value);
+
+	string getProcName();
+	int getTotalStmNo();
+	unordered_set<int> getReadStms();
+	unordered_set<int> getPrintStms();
+	unordered_set<int> getAssignStms();
+	unordered_set<int> getIfStms();
+	unordered_set<int> getWhileStms();
+	vector<string> getVariables();
+	vector<int> getConstants();
+
+private:
+	static string procName;
+	static vector<stmType> stmTypeList;
+	static vector<string> varList;
+	static vector<int> constList;
+	static unordered_set<int> readStmList;
+	static unordered_set<int> printStmList;
+	static unordered_set<int> assignStmList;
+	static unordered_set<int> ifStmList;
+	static unordered_set<int> whileStmList;
 };
