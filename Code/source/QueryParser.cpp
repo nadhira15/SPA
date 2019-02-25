3c0496bacfff822222fbb7b853c566b141fa5b11
nadhira15 <nadhira_salsabila@yahoo.co.id>
2019-02-25 15:19:17 +0800

    add validation logic

--- Code/source/QueryParser.cpp
+++ Code/source/QueryParser.cpp
@@ -6,19 +6,24 @@
 #include <vector>
 #include <algorithm>
 #include <unordered_set>
+#include <unordered_map>
 
 using namespace std;
 
 #include "QueryParser.h"
+#include "LexicalToken.h"
 
 const string whitespace = " \f\n\r\t\v";
-const unordered_set<string> validVarType = { "stmt", "read", "print", "while", "if", "assign", "variable", "constant", "procedure" };
+const unordered_set<string> validVarType = { "stmt", "read", "print", "call", "while", "if", "assign", "variable", "constant", "procedure" };
 
 string QueryParser::parse(string query) {
 	/*
 	Main parser function for input query.
 	Passes the following to query evaluator:
-	...
+	1. vector<pair<string, string>> declarations 
+	2. vector<string> selectedVar
+	3. vector<pair<string, pair<string, string>>> suchThatCondition
+	4. vector<pair<string, pair<string, string>>> patternCondition
 	*/
 
 	string errorString;
@@ -40,6 +45,11 @@ string QueryParser::parse(string query) {
 
 	// parsing declarations
 	vector<pair<string, string>> declarations = splitDeclarations(clauses);
+
+	unordered_map<string, string> declarationsMap;
+	for (int i = 0; i < declarations.size(); i++) {
+		declarationsMap.insert(make_pair(declarations[i].second, declarations[i].first));
+	}
 
 	// validating declarations
 	errorString = validateDeclarations(declarations);
@@ -81,19 +91,19 @@ string QueryParser::parse(string query) {
 	}
 
 	// validating 'Select' parameter
-	errorString = validateSelectedVar(selectedVar, declarations);
+	errorString = validateSelectedVar(selectedVar, declarationsMap);
 	if (errorString != "") {
 		return errorString;
 	}
 
 	// validating 'such that' parameter
-	errorString = validateSuchThatParam(suchThatCondition);
+	errorString = validateSuchThatParam(suchThatCondition, declarationsMap);
 	if (errorString != "") {
 		return errorString;
 	}
 
 	// validating 'pattern' parameter
-	errorString = validatePatternParam(patternCondition);
+	errorString = validatePatternParam(patternCondition, declarationsMap);
 	if (errorString != "") {
 		return errorString;
 	}
@@ -202,10 +212,13 @@ vector<pair<string, string>> QueryParser::splitDeclarations(vector<string> claus
 }
 
 string QueryParser::validateDeclarations(vector<pair<string, string>> declarations) {
-	// TODO: verify varName with lexical token
 
 	for (int i = 0; i < declarations.size(); i++) {
 		if (validVarType.find(declarations[i].first) == validVarType.end()) {
+			return "invalid query";
+		}
+
+		if (!LexicalToken::verifyName(declarations[i].second)) {
 			return "invalid query";
 		}
 	}
@@ -258,17 +271,103 @@ vector<pair<string, pair<string, string>>> QueryParser::splitPatternCondition(st
 	return output;
 }
 
-string QueryParser::validateSelectedVar(vector<string> selectedVar, vector<pair<string, string>> declarations) {
+string QueryParser::validateSelectedVar(vector<string> selectedVar, unordered_map<string, string> declarationsMap) {
+
+	for (int i = 0; i < selectedVar.size(); i++) {
+		if (!LexicalToken::verifyName(selectedVar[i])) {
+			return "invalid variable name";
+		}
+
+		if (declarationsMap.find(selectedVar[i]) == declarationsMap.end()) {
+			return "selected variable not found";
+		}
+	}
 
 	return "";
 }
 
-string QueryParser::validateSuchThatParam(vector<pair<string, pair<string, string>>> param) {
+string QueryParser::validateSuchThatParam(vector<pair<string, pair<string, string>>> param, unordered_map<string, string> declarationsMap) {
+
+	unordered_set<string> validRelation = { "Parent", "Parent*", "Follows", "Follows*", "Uses", "Modifies" };
+	unordered_set<string> validArgs = { "stmt", "read", "print", "while", "if", "assign" };
+	unordered_set<string> validFirstArgsParent = { "stmt", "while", "if" };
+	unordered_set<string> validFirstArgsUses = { "stmt", "print", "while", "if", "procedure", "call" };
+	unordered_set<string> validFirstArgsModifies = { "stmt", "read", "while", "if", "procedure", "call" };
+
+	for (int i = 0; i < param.size(); i++) {
+		string relation = param[i].first;
+		string firstArgs = param[i].second.first;
+		string secondArgs = param[i].second.second;
+
+		if (validRelation.find(relation) == validRelation.end()) {
+			return "invalid query";
+		}
+
+		if (relation == "Uses") {
+			if (firstArgs != "_" && !LexicalToken::verifyInteger(firstArgs) && !LexicalToken::verifyName(firstArgs) &&
+				validFirstArgsUses.find(firstArgs) == validFirstArgsUses.end()) {
+				return "invalid query";
+			}
+
+			if (secondArgs != "_" && !LexicalToken::verifyInteger(secondArgs) && !LexicalToken::verifyName(secondArgs)) {
+				return "invalid query";
+			}
+		}
+		else if (relation == "Modifies") {
+			if (firstArgs != "_" && !LexicalToken::verifyInteger(firstArgs) && !LexicalToken::verifyName(firstArgs) &&
+				validFirstArgsModifies.find(firstArgs) == validFirstArgsModifies.end()) {
+				return "invalid query";
+			}
+
+			if (secondArgs != "_" && !LexicalToken::verifyInteger(secondArgs) && !LexicalToken::verifyName(secondArgs)) {
+				return "invalid query";
+			}
+		}
+		else if (relation == "Parent" || relation == "Parent*") {
+			if (firstArgs != "_" && !LexicalToken::verifyInteger(firstArgs) &&
+				validFirstArgsParent.find(firstArgs) == validFirstArgsParent.end()) {
+				return "invalid query";
+			}
+
+			if (secondArgs != "_" && !LexicalToken::verifyInteger(secondArgs) && 
+				validArgs.find(firstArgs) == validArgs.end()) {
+				return "invalid query";
+			}
+		}
+		else {
+			// relation == Follows | Follows*
+
+			if (firstArgs != "_" && !LexicalToken::verifyInteger(firstArgs) &&
+				validArgs.find(firstArgs) == validArgs.end()) {
+				return "invalid query";
+			}
+
+			if (secondArgs != "_" && !LexicalToken::verifyInteger(secondArgs) &&
+				validArgs.find(firstArgs) == validArgs.end()) {
+				return "invalid query";
+			}
+		}
+	}
 
 	return "";
 }
 
-string QueryParser::validatePatternParam(vector<pair<string, pair<string, string>>> param) {
+string QueryParser::validatePatternParam(vector<pair<string, pair<string, string>>> param, unordered_map<string, string> declarationsMap) {
+
+	for (int i = 0; i < param.size(); i++) {
+		string stmt = param[i].first;
+
+		if (!LexicalToken::verifyName(stmt)) {
+			return "invalid variable name";
+		}
+
+		if (declarationsMap.find(stmt) == declarationsMap.end()) {
+			return "statement not found";
+		}
+		else if ((declarationsMap.find(stmt))->second != "assign") {
+			return "statement type is not assign";
+		}
+	}
 
 	return "";
 }
