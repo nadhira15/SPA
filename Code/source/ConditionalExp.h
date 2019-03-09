e57e5707b17df5fdc9b51ba8479cb186677fe506
APPLETEA\Huijia <hjc2304@gmail.com>
2019-03-09 14:02:27 +0800

    no message

--- Code/source/ConditionalExp.h
+++ Code/source/ConditionalExp.h
@@ -2,10 +2,10 @@
 #include<string>
 #include<vector>
 
-
 namespace ConditionalExp {
 
 	bool verifyConditionalExp(const std::string statement);
 	std::vector<std::string> getVariables();
 	std::vector<std::string> getConstants();
+
 };
