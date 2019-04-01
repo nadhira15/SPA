cdccd5e03d98facbbc46a1332e7151e78fadd32e
kosyoz <31100899+kosyoz@users.noreply.github.com>
2019-04-01 16:52:53 +0800

    Add Next* for two Synonym

--- Code/source/RunTimeDesignExtractor.h
+++ Code/source/RunTimeDesignExtractor.h
@@ -7,8 +7,9 @@ public:
 	static unordered_set<int> extractNextStar(int stmt);
 	static unordered_set<int> extractPreviousStar(int stmt);
 	static bool extractNextStarPair(int start, int end);
-	static void DFSRecursiveNext(int procedure, unordered_set<int> &visitedStmts);
-	static void DFSRecursivePrevious(int procedure, unordered_set<int> &visitedStmts);
+	static unordered_set<pair<int, int>, intPairhash> getNextStarPairs();
+	static void DFSRecursiveNext(int procedure, unordered_set<int> &visitedStmts, unordered_set<int>& stmtsAfter);
+	static void DFSRecursivePrevious(int procedure, unordered_set<int> &visitedStmts, unordered_set<int> &stmtsBefore);
 
 private:
 	static PKB pkb;
