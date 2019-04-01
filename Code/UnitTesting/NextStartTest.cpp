9352f0da936bcb6a70db60fa3f9f354506caa1c5
kosyoz <31100899+kosyoz@users.noreply.github.com>
2019-04-01 16:53:09 +0800

    Add Test Cases for Next Star

--- Code/UnitTesting/NextStartTest.cpp
+++ Code/UnitTesting/NextStartTest.cpp
@@ -22,5 +22,63 @@ namespace UnitTesting
 			bool result = storage.hasNextStarPair(1, 4);
 			Assert::AreEqual(true, result);
 		}
+		TEST_METHOD(testAllLnAfter)
+		{
+			PKB storage = PKB();
+			storage.addNext(1, 2);
+			storage.addNext(2, 3);
+			storage.addNext(3, 4);
+			storage.addNext(4, 5);
+			storage.addNext(2, 5);
+			unordered_set<int> expected;
+			expected.insert(2);
+			expected.insert(3);
+			expected.insert(4);
+			expected.insert(5);
+			unordered_set<int> result = storage.getAllLnAfter(1);
+			bool output;
+			if (result == expected) {
+				output = true;
+			}
+			Assert::AreEqual(true, output);
+		}
+		TEST_METHOD(testNextStarPair)
+		{
+			PKB storage = PKB();
+			storage.addStatement(1, assign);
+			storage.addStatement(2, whileStm);
+			storage.addStatement(3, assign);
+			storage.addStatement(4, assign);
+			storage.addStatement(5, assign);
+			storage.addNext(1, 2);
+			storage.addNext(2, 3);
+			storage.addNext(3, 4);
+			storage.addNext(4, 5);
+			storage.addNext(4, 2);
+			storage.addNext(2, 5);
+			unordered_set<pair<int, int>, intPairhash> expected;
+			expected.emplace(1, 2);
+			expected.emplace(1, 3);
+			expected.emplace(1, 4);
+			expected.emplace(1, 5);
+			expected.emplace(2, 2);
+			expected.emplace(2, 3);
+			expected.emplace(2, 4);
+			expected.emplace(2, 5);
+			expected.emplace(3, 3);
+			expected.emplace(3, 4);
+			expected.emplace(3, 5);
+			expected.emplace(3, 2);
+			expected.emplace(4, 5);
+			expected.emplace(4, 2);
+			expected.emplace(4, 3);
+			expected.emplace(4, 4);
+			unordered_set<pair<int, int>, intPairhash> result = storage.getNextStarPairs();
+			bool output;
+			if (result == expected) {
+				output = true;
+			}
+			Assert::AreEqual(true, output);
+		}
 	};
 }
