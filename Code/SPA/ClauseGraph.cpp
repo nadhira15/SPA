623f6ffa4e85ad52f9c97f1f62a07458695a7e04
APPLETEA\Huijia <hjc2304@gmail.com>
2019-04-02 23:28:54 +0800

    fixed logic bugs

--- Code/SPA/ClauseGraph.cpp
+++ Code/SPA/ClauseGraph.cpp
@@ -1,9 +1,10 @@
 #include "ClauseGraph.h"
 
-ClauseGraph::ClauseGraph(std::vector<std::pair<std::string, std::pair<std::string, std::string>>> st,
-	std::vector<std::pair<std::string, std::string>> w,
+ClauseGraph::ClauseGraph(
+	std::vector<std::pair<std::string, std::pair<std::string, std::string>>> st,
+	std::vector<std::pair<std::string, std::pair<std::string, std::string>>> w,
 	std::vector<std::pair<std::string, std::pair<std::string, std::string>>> p, 
-	std::vector<std::string> select) {
+	std::unordered_set<std::string> select) {
 	suchThatClauses = st;
 	withClauses = w;
 	patternClauses = p;
@@ -15,69 +16,82 @@ void ClauseGraph::groupClause() {
 	groupByClauseType(st);
 	groupByClauseType(w);
 	groupByClauseType(p);
-	int check = suchThatClauses.size() + withClauses.size() + patternClauses.size();
 	std::pair<clauseType, int> cl;
-	bool trivial;
-	while (check != 0) {
-		cl = getFirstCl(); //function for the more efficient? get from PKB statistics
-		trivial = mapClauses(std::unordered_set<std::pair<std::string, std::pair <std::string, std::string >>>(), cl, false);
-		if (trivial) {
-			//add to non trivial sets
+	bool isTrivial;
+	while (!clSet.empty()) {
+		//function for the more efficient? get from PKB statistics or pq
+		cl = *clSet.begin();
+		isTrivial = mapClauses(cl, true);
+		if (isTrivial) {
+			trivial.push_back(graph);
+		} else {
+			nontrivial.push_back(graph);
 		}
-		else {
-			//add to trivial set
-		}
+		graph.clear();
 	}
 }
 
+/*
+Creates a hashmap of syn-clause and clause-syn, which will be used for graph traversal
+*/
 void ClauseGraph::groupByClauseType(clauseType t) {
 	std::vector<std::string> synLst;
 	if (t == st) {
 		for (int i = 0; i < suchThatClauses.size(); i++) {
 			synLst = extractSuchThatSyn(i);
-			mapSynonyms(synLst, std::make_pair(st, i));
+			createMaps(synLst, std::make_pair(st, i));
 		}
 	} else if (t == p) {
 		for (int i = 0; i < patternClauses.size(); i++) {
 			synLst = extractPatternSyn(i);
-			mapSynonyms(synLst, std::make_pair(p, i));
+			createMaps(synLst, std::make_pair(p, i));
 		}
 	} else {
 		for (int i = 0; i < withClauses.size(); i++) {
 			synLst = extractWithSyn(i);
-			mapSynonyms(synLst, std::make_pair(w, i));
+			createMaps(synLst, std::make_pair(w, i));
 		}
 	}
 }
 
-void ClauseGraph::mapSynonyms(std::vector<std::string> synLst, std::pair<clauseType, int> cl) {
+void ClauseGraph::createMaps(std::vector<std::string> synLst, std::pair<clauseType, int> cl) {
 	clMap[cl] = synLst;
 	for (std::vector<std::string>::iterator it = synLst.begin(); it != synLst.end(); ++it) {
-		if (synMap.find(*it) == synMap.end()) {
-			//synMap[*it] = std::vector<std::pair<clauseType, int>>();
-			synMap[*it].push_back(cl);
-		} else {
-			//group index for cl0 = group index for cl1
-			synMap.at[*it].insert(cl);
-		}
+		synMap[*it].push_back(cl);
 	}
 }
 
-bool ClauseGraph::mapClauses(std::vector <std::pair<clauseType, int>> set, std::pair<clauseType, int> cl, bool trivial) {
-	std::vector<std::string> synLst = clMap[cl];
-	for (std::vector<std::string>::iterator it = synLst.begin(); it != synLst.end(); ++it) {
-		//choose the more optimal clause
-		//
-		//check if *it is part of select
-		if (true) {
-			trivial = true;
+bool ClauseGraph::mapClauses(std::pair<clauseType, int> cl, bool isTrivial) {
+	std::pair<std::string, std::pair<std::string, std::string>> clause;
+	clSet.erase(cl); //clause removed from bookkeeping sets
+	for (std::vector<std::string>::iterator it = clMap[cl].begin(); it != clMap[cl].end(); ++it) {
+		if (synSet.find(*it) != synSet.end()) {
+			if (cl.first == st) {
+				clause = suchThatClauses.at(cl.second);
+			} else if (cl.first == p) {
+				clause = patternClauses.at(cl.second);
+			} else {
+				clause = withClauses.at(cl.second);
+			}
+			graph.push_back(clause);
+			isTrivial = mapSynonym(*it, isTrivial);
 		}
-		set.push_back(cl);
-		cl = getOptimal(synMap[*it])
-		synMap[*it].remove(cl);
-		clMap[cl].remove(*it);
-		mapClauses(set, cl);
 	}
+	return isTrivial;
+}
+
+bool ClauseGraph::mapSynonym(std::string syn, bool isTrivial) {
+	//check if syn is part of select if group is trivial
+	if (isTrivial && (selectClause.find(syn) != selectClause.end())) {
+		isTrivial = false;
+	}
+	synSet.erase(syn); //synonym removed from bookkeeping sets
+	for (std::vector<std::pair<clauseType, int>>::iterator it = synMap[syn].begin(); it != synMap[syn].end(); ++it) {
+		if (clSet.find(*it) != clSet.end()) { //check if clause is already processed
+			isTrivial = mapClauses(*it, isTrivial);
+		}
+	}
+	return isTrivial;
 }
 
 std::vector<std::string> ClauseGraph::extractSuchThatSyn(int index) {
@@ -100,8 +114,8 @@ std::vector<std::string> ClauseGraph::extractPatternSyn(int index) {
 
 std::vector<std::string> ClauseGraph::extractWithSyn(int index) {
 	std::vector<std::string> synLst;
-	std::string f = withClauses.at(index).first;
-	std::string s = withClauses.at(index).second;
+	std::string f = withClauses.at(index).second.first;
+	std::string s = withClauses.at(index).second.second;
 	if (isWithSynonym(f)) synLst.push_back(f);
 	if (isWithSynonym(s)) synLst.push_back(s);
 	return synLst;
@@ -123,3 +137,9 @@ bool isWithSynonym(std::string str) {
 	return res;
 }
 
+std::vector<std::vector<std::pair<std::string, std::pair<std::string, std::string>>>> ClauseGraph::getTrivial() {
+	return trivial;
+}
+std::vector<std::vector<std::pair<std::string, std::pair<std::string, std::string>>>> ClauseGraph::getNonTrivial() {
+	return nontrivial;
+}
--- Code/SPA/ClauseGraph.h
+++ Code/SPA/ClauseGraph.h
@@ -7,28 +7,32 @@
 
 class ClauseGraph {
 private:
-	enum clauseType { st, w, p };
-	int counter = 0;
+	enum clauseType { st, w, p }; //clauses are represented by <clauseType,index>
 	std::vector<std::pair<std::string, std::pair<std::string, std::string>>> suchThatClauses;
-	std::vector<std::pair<std::string, std::string>> withClauses;
+	std::vector<std::pair<std::string, std::pair<std::string, std::string>>> withClauses;
 	std::vector<std::pair<std::string, std::pair<std::string, std::string>>> patternClauses;
-	std::vector<std::string> selectClause;
+	std::unordered_set<std::string> selectClause;
 	std::unordered_map<std::string, std::vector<std::pair<clauseType,int>>> synMap;
 	std::unordered_map<std::pair<clauseType, int>, std::vector<std::string>> clMap;
-	std::unordered_map<int, std::unordered_set<std::pair<std::string, std::pair<std::string, std::string>>>> stMap;
-	std::unordered_map<int, std::unordered_set<std::pair<std::string, std::string>>> wMap;
-	std::unordered_map<int, std::unordered_set<std::pair<std::string, std::pair<std::string, std::string>>>> pMap;
+	std::vector<std::vector<std::pair<std::string, std::pair<std::string, std::string>>>> trivial;
+	std::vector<std::vector<std::pair<std::string, std::pair<std::string, std::string>>>> nontrivial;
+	std::vector<std::pair<std::string, std::pair<std::string, std::string>>> graph;
+	std::unordered_set<std::string> synSet;
+	std::unordered_set<std::pair<clauseType,int>> clSet;
 	void groupByClauseType(clauseType t);
-	std::vector<std::string> ClauseGraph::extractSuchThatSyn(int index);
-	std::vector<std::string> ClauseGraph::extractWithSyn(int index);
-	std::vector<std::string> ClauseGraph::extractPatternSyn(int index);
-	void mapSynonyms(std::vector<std::string> synLst, std::pair<clauseType, int> cl);
-	bool mapClauses(std::unordered_set < std::pair < std::string, std::pair < std::string, std::string >> >, std::pair<clauseType, int>);
+	std::vector<std::string> extractSuchThatSyn(int index);
+	std::vector<std::string> extractWithSyn(int index);
+	std::vector<std::string> extractPatternSyn(int index);
+	void createMaps(std::vector<std::string> synLst, std::pair<clauseType, int> cl);
+	bool mapClauses(std::pair<clauseType, int> cl, bool trivial);
+	bool mapSynonym(std::string syn, bool trivial);
 public:
 	//constructor
 	ClauseGraph(std::vector<std::pair<std::string, std::pair<std::string, std::string>>> st,
-		 std::vector<std::pair<std::string, std::string>> w, 
-		 std::vector<std::pair<std::string, std::pair<std::string, std::string>>> p, 
-		 std::vector<std::string> select);
+		std::vector<std::pair<std::string, std::pair<std::string, std::string>>> w,
+		std::vector<std::pair<std::string, std::pair<std::string, std::string>>> p,
+		std::unordered_set<std::string> select);
 	void groupClause();
+	std::vector<std::vector<std::pair<std::string, std::pair<std::string, std::string>>>> getTrivial();
+	std::vector<std::vector<std::pair<std::string, std::pair<std::string, std::string>>>> getNonTrivial();
 };
