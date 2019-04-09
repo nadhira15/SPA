3c4364167b3f4d2856787d4f70023fd0dca0c78f
Randy Pang Chung <35571456+randypx@users.noreply.github.com>
2019-04-09 13:48:12 +0800

    UnitTesting & Affects/* Integration (#185)
    * add erase method to CtrlVarStorage
    
    * added a special 'nonExistant' stm type for when getStmType fails to find a stm
    
    * Additional check for getProcOfStm method and some comment changes
    
    * have the storage be erased before each test
    
    * Updated test cases and their relevant stubs
    
    * Created unit test for PKB and CtrlVarStorage
    
    * updated PKB erase method
    
    * Added back comments
    
    * integrated Affects / Affects* in RTDE

---
+++ Code/UnitTesting/PKB/CtrlVarStorageStub.h
@@ -0,0 +1,51 @@
+#pragma once
+
+#include "CtrlVarStorage.h"
+
+/*
+
+	procedure fruit {
+1		read apple;
+2		call fruit2;
+3		print banana;
+4		apple = cherry + 10 * banana;
+5		if (apple <= 0) then {
+6			while ( durian > cherry ) {
+7				durian = grapes + banana;
+			}
+		} else {
+8			figs = apple * melon / cherry;
+9			call fruit3;
+		}
+	}
+
+	procedure fruit2 {
+10		pineapple = apple + melon;
+11		while (10 > banana) {
+12			call fruit3;
+		}
+	}
+
+	procedure fruit3 {
+13		print melon;
+14		call fruit4;
+	}
+
+	procedure fruit4 {
+15		pear = jackfruit + apple;
+	}
+
+*/
+
+/*
+if / while pattern
+store.addIfControlVariable(5, "apple");
+store.addWhileControlVariable(6, "durian");
+store.addWhileControlVariable(6, "cherry");
+store.addWhileControlVariable(11, "banana");
+*/
+
+std::unordered_map<std::string, std::unordered_set<int>> ifVariableToStm = { {"apple", {5}} };
+
+std::unordered_map<std::string, std::unordered_set<int>> whileVariableToStm = { {"cherry", {6}},
+{"durian", {6}}, {"banana", {11}} };
---
+++ Code/UnitTesting/PKB/CtrlVarStorageTest.cpp
@@ -0,0 +1,73 @@
+#include "stdafx.h"
+#include "CppUnitTest.h"
+#include "CtrlVarStorageStub.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTesting
+{
+	TEST_CLASS(CtrlVarStorageTest)
+	{
+		TEST_METHOD_INITIALIZE(CallStorage_Initialize)
+		{
+			CtrlVarStorage store;
+			store.erase();
+			store.addIfControlVariable(5, "apple");
+			store.addWhileControlVariable(6, "durian");
+			store.addWhileControlVariable(6, "cherry");
+			store.addWhileControlVariable(11, "banana");
+		}
+
+		TEST_METHOD(getAllIfWithControls_Correct)
+		{
+			Assert::IsTrue(CtrlVarStorage().getAllIfWithControls() == std::unordered_set<int>{5});
+		}
+
+		TEST_METHOD(getAllWhileWithControls_Correct)
+		{
+			Assert::IsTrue(CtrlVarStorage().getAllWhileWithControls() == std::unordered_set<int>{6, 11});
+		}
+
+		TEST_METHOD(getIfStmWithControlVariable_ExistingVariable_Success)
+		{
+			Assert::IsTrue(CtrlVarStorage().getIfStmWithControlVariable("apple") ==
+						   std::unordered_set<int>{5});
+		}
+
+		TEST_METHOD(getIfStmWithControlVariable_NonExistingVariable_Fail)
+		{
+			Assert::IsTrue(CtrlVarStorage().getIfStmWithControlVariable("nonExistant") ==
+						   std::unordered_set<int>{});
+		}
+
+		TEST_METHOD(getWhileStmWithControlVariable_ExistingVariable_Success)
+		{
+			Assert::IsTrue(CtrlVarStorage().getWhileStmWithControlVariable("cherry") ==
+						   std::unordered_set<int>{6});
+		}
+
+		TEST_METHOD(getWhileStmWithControlVariable_NonExistingVariable_Fail)
+		{
+			Assert::IsTrue(CtrlVarStorage().getWhileStmWithControlVariable("nonExistant") ==
+						   std::unordered_set<int>{});
+		}
+
+		TEST_METHOD(getIfStmControlVariablePair_Correct)
+		{
+			Assert::IsTrue(CtrlVarStorage().getIfStmControlVariablePair() ==
+						   std::unordered_set<std::pair<int, std::string>, intStringhash>
+								{std::pair<int, std::string>(5, "apple")});
+		}
+
+		TEST_METHOD(getWhileStmControlVariablePair_Correct)
+		{
+			Assert::IsTrue(CtrlVarStorage().getWhileStmControlVariablePair() ==
+						   std::unordered_set<std::pair<int, std::string>, intStringhash>
+							{ std::pair<int, std::string>(6, "durian"),
+								std::pair<int, std::string>(6, "cherry"),
+								std::pair<int, std::string>(11, "banana") });
+		}
+
+	};
+}
+
