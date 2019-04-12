a190a52e823a41f13fda2512a1a30a717983f765
nadhira15 <nadhira_salsabila@yahoo.co.id>
2019-04-12 14:03:31 +0800

    add with queries and update batch file

--- Tests/system-tests-3/runAllQueries.bat
+++ Tests/system-tests-3/runAllQueries.bat
@@ -6,6 +6,9 @@ title system test 3 queries
 ..\..\Code\Debug\AutoTester sourceAlphanumeric.txt Calls.txt out-Calls.xml
 ..\..\Code\Debug\AutoTester sourceAlphanumeric.txt CallsStar.txt out-CallsStar.xml
 
+..\..\Code\Debug\AutoTester sourceAlphanumeric.txt Next.txt out-Next.xml
+..\..\Code\Debug\AutoTester sourceAlphanumeric.txt NextStar.txt out-NextStar.xml
+
 echo FINISHED
 pause
 exit
