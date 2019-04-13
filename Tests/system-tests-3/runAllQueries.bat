bcf8b3e50744afde89b26b9f874b3d8270f08fb0
Jackie Tan <free-soup@hotmail.com>
2019-04-13 18:02:53 +0800

    Add WhilePattern Queries for Test 3

--- Tests/system-tests-3/runAllQueries.bat
+++ Tests/system-tests-3/runAllQueries.bat
@@ -1,15 +1,20 @@
 @echo off
 title system test 3 queries
 
-..\..\Code\Debug\AutoTester sourceAlphanumeric.txt Affects.txt out-Affects.xml
+..\..\Code\Release\AutoTester sourceAlphanumeric.txt Affects.txt out-Affects.xml
 
-..\..\Code\Debug\AutoTester sourceAlphanumeric.txt Calls.txt out-Calls.xml
-..\..\Code\Debug\AutoTester sourceAlphanumeric.txt CallsStar.txt out-CallsStar.xml
+..\..\Code\Release\AutoTester sourceAlphanumeric.txt Calls.txt out-Calls.xml
+..\..\Code\Release\AutoTester sourceAlphanumeric.txt CallsStar.txt out-CallsStar.xml
 
-..\..\Code\Debug\AutoTester sourceAlphanumeric.txt Next.txt out-Next.xml
-..\..\Code\Debug\AutoTester sourceAlphanumeric.txt NextStar.txt out-NextStar.xml
+..\..\Code\Release\AutoTester sourceAlphanumeric.txt Next.txt out-Next.xml
+..\..\Code\Release\AutoTester sourceAlphanumeric.txt NextStar.txt out-NextStar.xml
 
-..\..\Code\Debug\AutoTester sourceAlphanumeric.txt NoCondition.txt out-NoCondition.xml
+..\..\Code\Release\AutoTester sourceAlphanumeric.txt NoCondition.txt out-NoCondition.xml
+
+..\..\Code\Release\AutoTester sourceAlphanumeric.txt Boolean.txt out-Boolean.xml
+
+..\..\Code\Release\AutoTester sourceAlphanumeric.txt PatternWhile.txt out-PatternWhile.xml
+
 
 echo FINISHED
 pause
