179c28ff972d2fe5c0fe311894378f54a010a0b2
nadhira15 <nadhira_salsabila@yahoo.co.id>
2019-04-13 08:30:25 +0800

    Add affects* queries for system test 4

--- Tests/system-tests-4/runAllQueries.bat
+++ Tests/system-tests-4/runAllQueries.bat
@@ -2,6 +2,7 @@
 title system test 3 queries
 
 ..\..\Code\Debug\AutoTester SourceAffectsProcedure.txt Affects.txt out-Affects.xml
+..\..\Code\Debug\AutoTester SourceAffectsProcedure.txt AffectsStar.txt out-AffectsStar.xml
 
 echo FINISHED
 pause
